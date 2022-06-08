fa13ab94e5dccc499976a69fd8f3ec0dfe53c11a
jingrongchen <jingrong.chen@epfl.ch>
2022-06-08 23:07:52 +0200

    haha

--- couplet.py
+++ couplet.py
@@ -2,15 +2,17 @@
 from model import Model
 
 m = Model(
-        './data/train/in.txt',
-        './data/train/out.txt',
-        './data/test/in.txt',
-        './data/test/out.txt',
-        './data/vocabs',
+        '/data/train/in.txt',
+        '/data/train/out.txt',
+        '/data/test/in.txt',
+        '/data/test/out.txt',
+        '/data/vocabs',
         num_units=1024, layers=4, dropout=0.2,
         batch_size=32, learning_rate=0.001,
-        output_dir='./model/tf-lib/output_humor',
+        output_dir='/model/tf-lib/output_humor',
         restore_model=False)
 
 # m.train(5000000)
 m.train(5)
+
+# data/vocabs
